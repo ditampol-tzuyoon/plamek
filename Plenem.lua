daeb97781215eb04970fdbe586a015a4936d9c51
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2023-01-02 13:20:19 +0700

    Create Plenem.lua

---
+++ Plenem.lua
@@ -0,0 +1,18 @@
+ListPembeli = {
+    {iddc = "422670528483033088", nick = "OdStillAlive"},
+    {iddc = "514608997995773963", nick = "14Walthor"},
+    {iddc = "882370093957382184", nick = "Zunas"},
+    {iddc = "728628332416991373", nick = "mekiky"},
+    {iddc = "550658413315751936", nick = "MOVIKIS"},
+    {iddc = "316409421137838080", nick = "Pedescantik"},
+    {iddc = "453032872886927371", nick = "KOTYU"},
+    {iddc = "456612225130561537", nick = "ShidoTK"},
+    {iddc = "759384235113971715", nick = "MaunyaUang"},
+    {iddc = "961929886547734528", nick = "asiahae27"},
+    {iddc = "863396091868872714", nick = "RoyalFarmer89"},
+    {iddc = "261481844598505472", nick = "PENTLUME"},
+    {iddc = "336391397106253835", nick = "cuakscuaks26"},
+    {iddc = "994089791941513267", nick = "CieleSan"},
+    {iddc = "1019129204060717129", nick = "ZornController"},
+    {iddc = "262666680004640770", nick = "RLNT"}
+}
