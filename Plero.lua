b8709c4020aac03501b4efb70c0ebd5ae1a47059
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2023-01-03 12:14:50 +0700

    Update Plero.lua

--- Plero.lua
+++ Plero.lua
@@ -2,5 +2,12 @@ ListPembeli = {
     {iddc = "1001607668772900864", nick = "OdWasHerezz"},
     {iddc = "418412543808241674", nick = "axolqqr"},
     {iddc = "994089791941513267", nick = "CieleSan"},
-    {iddc = "551073614166884352", nick = "ZXCQQ"}
+    {iddc = "551073614166884352", nick = "ZXCQQ"},
+    {iddc = "336391397106253835", nick = "cuakscuaks26"},
+    {iddc = "882370093957382184", nick = "Zunas"},
+    {iddc = "942277519376252959", nick = "Surgpung200"},
+    {iddc = "550658413315751936", nick = "OVERATION"},
+    {iddc = "335422307197648896", nick = "Dcstzy15"},
+    {iddc = "211379917307248640", nick = "Gravell"},
+    {iddc = "316409421137838080", nick = "Gantengbau"}
  }
