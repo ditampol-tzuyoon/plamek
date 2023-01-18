c00bc038ab01e866165a8a9c42fda83b365f5259
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2023-01-18 23:47:14 +0700

    Update Plenem.lua

--- Plenem.lua
+++ Plenem.lua
@@ -86,7 +86,7 @@ ListPembeli = {
     {iddc = "614877382352044073", nick = "AJABAT100"},
     {iddc = "949474781290463294", nick = "KangCMDIdann"},
     {iddc = "1002625833191608400", nick = "Azumaga"},
-    {iddc = "358234096650158082", nick = "neweros26"},
+    {iddc = "358234096650158082", nick = "p1kypuck"},
     {iddc = "961591462947065907", nick = "dilacuans"},
     {iddc = "597036774342131733", nick = "PengabdiMonyet"},
     {iddc = "902144872096608277", nick = "LockMH"},
