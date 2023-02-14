e181a6501b67b506717161392369c433a3ff8bf8
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2023-02-14 17:59:26 +0700

    Update Plenem.lua

--- Plenem.lua
+++ Plenem.lua
@@ -138,5 +138,6 @@ ListPembeli = {
     {iddc = "898237069862789160", nick = "dontcryyy"},
     {iddc = "701342840696864798", nick = "DevMzn"},
     {iddc = "838439967176785961", nick = "eluzz"},
-    {iddc = "477078525896032257", nick = "SENSEIGULO"}   
+    {iddc = "477078525896032257", nick = "SENSEIGULO"},
+    {iddc = "540758447810150412", nick = "gormutia23"}
 }
