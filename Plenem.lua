e675f6c57d4bed7a3a7cf8bd1b686a5c7eac53e3
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2023-02-19 12:28:54 +0700

    Update Plenem.lua

--- Plenem.lua
+++ Plenem.lua
@@ -146,5 +146,6 @@ ListPembeli = {
     {iddc = "992036856336228392", nick = "FachrulStopIlegal"},
     {iddc = "754633913439354901", nick = "move6ahead"},
     {iddc = "438947974429671424", nick = "Shikifujiin"},
-    {iddc = "521657255339360256", nick = "FlyFirst11"}
+    {iddc = "521657255339360256", nick = "FlyFirst11"},
+    {iddc = "908009441213947905", nick = "gtsbot"}
 }
