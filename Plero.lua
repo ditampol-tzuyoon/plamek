fa98d820e1d5a117b4df78990db74ff55b766c5c
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2023-02-22 00:57:58 +0700

    Update Plero.lua

--- Plero.lua
+++ Plero.lua
@@ -128,5 +128,6 @@ ListPembeli = {
     {iddc = "908009441213947905", nick = "gtsbot"},
     {iddc = "948410200199684106", nick = "VVMI"},
     {iddc = "773490212859805728", nick = "Sugihpradita"},
-    {iddc = "538721748346994698", nick = "Jengkale"}
+    {iddc = "538721748346994698", nick = "Jengkale"},
+    {iddc = "956613892077928528", nick = "Milliylov"}
 }
