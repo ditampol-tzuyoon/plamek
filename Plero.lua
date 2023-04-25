7340638b1591abbeb333986ca01e5105e3b490f3
ditampol-tzuyoon <72374101+ditampol-tzuyoon@users.noreply.github.com>
2023-04-25 13:13:22 +0800

    Update Plero.lua

--- Plero.lua
+++ Plero.lua
@@ -142,7 +142,6 @@ ListPembeli = {
     {iddc = "811237455638167615", nick = "hklmopnk"},
     {iddc = "884499020817506314", nick = "lemon125"},
     {iddc = "954387042966569051", nick = "BastianNeedBGL"},
-    {iddc = "710530597470208080", nick = "qwlopq12"},
     {iddc = "834445409106526230", nick = "LIUKANGDF7"},
     {iddc = "391458140551512085", nick = "AyangAmanda"},
     {iddc = "962536780698120233", nick = "triplewaluh2"},
